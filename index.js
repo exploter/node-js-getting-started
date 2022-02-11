d4d59c6851a8f066a198785792304fbc0f219cd0
Menashe Avramov <exploter@gmail.com>
2022-02-11 04:10:58 +0200

    aaa

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 //  .get('/', (req, res) => res.render('pages/index'))
-  res.redirect(301, 'http://example.com')
+//  .res.redirect(301, 'http://example.com')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  response.writeHead(301,
+    {Location: 'http://whateverhostthiswillbe:8675/'+newRoom}
+  );
+  response.end();
+  
